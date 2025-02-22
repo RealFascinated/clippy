890a69a37e7a039d135b566cf9fdc03908d27005
Lee <liam@fascinated.cc>
2025-02-22 11:17:00 +0000

    fix?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: "standalone",
+  poweredByHeader: false,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
