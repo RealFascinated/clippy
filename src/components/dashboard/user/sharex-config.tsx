0566b49e4295de2418ee42527b3d4c434519369b
Rainnny7 <braydonrainnny@gmail.com>
2025-02-27 17:38:16 -0500

    fix the width of the sharex configs component

--- src/components/dashboard/user/sharex-config.tsx
+++ src/components/dashboard/user/sharex-config.tsx
@@ -3,32 +3,32 @@ import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
 type ShareXConfigProps = {
-  uploadToken?: string | null;
+	uploadToken?: string | null;
 };
 
 export default function ShareXConfig({ uploadToken }: ShareXConfigProps) {
-  return (
-    <Card className="w-full">
-      <CardTitle>Configs</CardTitle>
-      <CardContent className="p-3">
-        {uploadToken ? (
-          <span className="text-muted-foreground">
-            Download a config for an upload client
-          </span>
-        ) : (
-          <span>
-            You do not have an upload token. Please generate one to download a
-            config.
-          </span>
-        )}
-      </CardContent>
-      <CardFooter>
-        {uploadToken && (
-          <Link href="/api/user/config/sharex" prefetch={false}>
-            <Button className="w-fit">Download ShareX</Button>
-          </Link>
-        )}
-      </CardFooter>
-    </Card>
-  );
+	return (
+		<Card className="w-full">
+			<CardTitle>Configs</CardTitle>
+			<CardContent className="max-w-sm p-3">
+				{uploadToken ? (
+					<span className="text-muted-foreground">
+						Download a config for an upload client
+					</span>
+				) : (
+					<span>
+						You do not have an upload token. Please generate one to download a
+						config.
+					</span>
+				)}
+			</CardContent>
+			<CardFooter>
+				{uploadToken && (
+					<Link href="/api/user/config/sharex" prefetch={false}>
+						<Button className="w-fit">Download ShareX</Button>
+					</Link>
+				)}
+			</CardFooter>
+		</Card>
+	);
 }
