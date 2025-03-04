2eb1fe06e6b2ede5ef0ce30858669b28d33d4aed
Lee <liam@fascinated.cc>
2025-03-04 13:36:09 +0000

    move logging to the server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import next from "next";
 import { parse } from "url";
 import TasksManager from "./tasks/tasks-manager";
 import { env } from "./lib/env";
+import { isProduction } from "./lib/utils/utils";
+import Logger from "./lib/logger";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = env.NEXT_PUBLIC_APP_ENV !== "production";
@@ -10,9 +12,18 @@ const app = next({ dev, turbopack: true });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(async (req, res) => {
     const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
+
+    const before = Date.now();
+    await handle(req, res, parsedUrl);
+
+    // Log requests in production
+    if (isProduction()) {
+      Logger.info(
+        `${req.method} ${parsedUrl.pathname}${parsedUrl.search} in ${Date.now() - before}ms`
+      );
+    }
   }).listen(port);
 
   new TasksManager();
