9152fa0b9f9e815e6e68866763d534ff5894af9d
Rainnny7 <braydonrainnny@gmail.com>
2025-03-05 21:00:46 -0500

    uh i forgot

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,76 +8,76 @@ import SidebarToggle from "./dashboard/sidebar/sidebar-toggle";
 import ProfileButton from "./user/profile-button";
 
 type Item = {
-  name: string;
-  icon?: ReactNode;
-  href: string;
-  className?: string;
+	name: string;
+	icon?: ReactNode;
+	href: string;
+	className?: string;
 };
 
 const items: Item[] = [];
 
 function NavButton({ name, icon, href }: Item) {
-  return (
-    <Link
-      href={href}
-      className="flex gap-2 hover:opacity-80 transition-all transform-gpu"
-      prefetch={false}
-    >
-      {icon}
-      {name}
-    </Link>
-  );
+	return (
+		<Link
+			href={href}
+			className="flex gap-2 hover:opacity-80 transition-all transform-gpu"
+			prefetch={false}
+		>
+			{icon}
+			{name}
+		</Link>
+	);
 }
 
 export default async function Navbar() {
-  return (
-    <header className="fle sticky flex top-0 z-50 justify-center items-center bg-card/60 backdrop-blur-md border border-border gap-3 px-4">
-      <SidebarToggle />
+	return (
+		<header className="fle sticky flex top-0 z-50 justify-center items-center bg-card/60 backdrop-blur-md border border-border gap-3 px-4 select-none">
+			<SidebarToggle />
 
-      <div className="flex max-w-7xl items-center h-[var(--header-height)] w-full justify-between">
-        <div className="flex gap-10 items-center">
-          {/* Website */}
-          <Link
-            href="/"
-            className="flex items-center gap-2 font-bold hover:opacity-80 transition-all transform-gpu"
-            prefetch={false}
-            draggable={false}
-          >
-            <Image
-              src={env.NEXT_PUBLIC_WEBSITE_LOGO}
-              alt={`${env.NEXT_PUBLIC_WEBSITE_NAME} Logo`}
-              width={20}
-              height={20}
-              draggable={false}
-              unoptimized
-            />
-            <span>{env.NEXT_PUBLIC_WEBSITE_NAME}</span>
-          </Link>
+			<div className="flex max-w-7xl items-center h-[var(--header-height)] w-full justify-between">
+				<div className="flex gap-10 items-center">
+					{/* Website */}
+					<Link
+						href="/"
+						className="flex items-center gap-2 font-bold hover:opacity-80 transition-all transform-gpu"
+						prefetch={false}
+						draggable={false}
+					>
+						<Image
+							src={env.NEXT_PUBLIC_WEBSITE_LOGO}
+							alt={`${env.NEXT_PUBLIC_WEBSITE_NAME} Logo`}
+							width={20}
+							height={20}
+							draggable={false}
+							unoptimized
+						/>
+						<span>{env.NEXT_PUBLIC_WEBSITE_NAME}</span>
+					</Link>
 
-          {/* Links */}
-          <div className="flex gap-4 items-center">
-            {items.map((item, index) => {
-              return (
-                <NavButton
-                  key={index}
-                  name={item.name}
-                  icon={item.icon}
-                  href={item.href}
-                  className={item.className}
-                />
-              );
-            })}
-          </div>
-        </div>
+					{/* Links */}
+					<div className="flex gap-4 items-center">
+						{items.map((item, index) => {
+							return (
+								<NavButton
+									key={index}
+									name={item.name}
+									icon={item.icon}
+									href={item.href}
+									className={item.className}
+								/>
+							);
+						})}
+					</div>
+				</div>
 
-        {/* Auth / Dashboard */}
-        <NotSignedIn>
-          <NavButton name="Login" className="h-8" href="/auth/login" />
-        </NotSignedIn>
-        <SignedIn>
-          <ProfileButton />
-        </SignedIn>
-      </div>
-    </header>
-  );
+				{/* Auth / Dashboard */}
+				<NotSignedIn>
+					<NavButton name="Login" className="h-8" href="/auth/login" />
+				</NotSignedIn>
+				<SignedIn>
+					<ProfileButton />
+				</SignedIn>
+			</div>
+		</header>
+	);
 }
--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -6,105 +6,106 @@ import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils/utils";
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
-  <nav
-    role="navigation"
-    aria-label="pagination"
-    className={cn(className)}
-    {...props}
-  />
+	<nav
+		role="navigation"
+		aria-label="pagination"
+		className={cn(className)}
+		{...props}
+	/>
 );
 Pagination.displayName = "Pagination";
 
 const PaginationContent = ({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<"ul">) => (
-  <ul
-    className={cn("flex flex-row items-center gap-1", className)}
-    {...props}
-  />
+	<ul
+		className={cn("flex flex-row items-center gap-1", className)}
+		{...props}
+	/>
 );
 
 const PaginationItem = ({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<"li">) => (
-  <li className={cn("", className)} {...props} />
+	<li className={cn("", className)} {...props} />
 );
 
 type PaginationLinkProps = {
-  isActive?: boolean;
+	isActive?: boolean;
 } & React.ComponentProps<"a">;
 
 const PaginationLink = ({
-  className,
-  isActive,
-  ...props
+	className,
+	isActive,
+	...props
 }: PaginationLinkProps) => (
-  <a
-    aria-current={isActive ? "page" : undefined}
-    className={cn(
-      buttonVariants({
-        variant: isActive ? "outline" : "ghost",
-        size: "icon",
-      }),
-      className,
-      "min-w-fit px-2"
-    )}
-    {...props}
-  />
+	<a
+		aria-current={isActive ? "page" : undefined}
+		className={cn(
+			buttonVariants({
+				variant: isActive ? "outline" : "ghost",
+				size: "icon",
+			}),
+			className,
+			"min-w-fit px-2"
+		)}
+		draggable={false}
+		{...props}
+	/>
 );
 PaginationLink.displayName = "PaginationLink";
 
 const PaginationPrevious = ({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<typeof PaginationLink>) => (
-  <PaginationLink
-    aria-label="Go to previous page"
-    className={cn("gap-1", className)}
-    {...props}
-  >
-    <ChevronLeftIcon className="h-4 w-4" />
-  </PaginationLink>
+	<PaginationLink
+		aria-label="Go to previous page"
+		className={cn("gap-1", className)}
+		{...props}
+	>
+		<ChevronLeftIcon className="h-4 w-4" />
+	</PaginationLink>
 );
 PaginationPrevious.displayName = "PaginationPrevious";
 
 const PaginationNext = ({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<typeof PaginationLink>) => (
-  <PaginationLink
-    aria-label="Go to next page"
-    className={cn("gap-1", className)}
-    {...props}
-  >
-    <ChevronRightIcon className="h-4 w-4" />
-  </PaginationLink>
+	<PaginationLink
+		aria-label="Go to next page"
+		className={cn("gap-1", className)}
+		{...props}
+	>
+		<ChevronRightIcon className="h-4 w-4" />
+	</PaginationLink>
 );
 PaginationNext.displayName = "PaginationNext";
 
 const PaginationEllipsis = ({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<"span">) => (
-  <span
-    aria-hidden
-    className={cn("flex h-9 w-5 items-center justify-center", className)}
-    {...props}
-  >
-    <DotsHorizontalIcon className="h-4 w-4" />
-    <span className="sr-only">More pages</span>
-  </span>
+	<span
+		aria-hidden
+		className={cn("flex h-9 w-5 items-center justify-center", className)}
+		{...props}
+	>
+		<DotsHorizontalIcon className="h-4 w-4" />
+		<span className="sr-only">More pages</span>
+	</span>
 );
 PaginationEllipsis.displayName = "PaginationEllipsis";
 
 export {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
 };
