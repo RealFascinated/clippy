fa2e57bd39575ae7a8fcc0ae7bc90f63df59aff3
Rainnny7 <braydonrainnny@gmail.com>
2025-03-07 01:51:57 -0500

    fix no upload token appearing as [object Object]

--- src/components/sensitive-input.tsx
+++ src/components/sensitive-input.tsx
@@ -4,26 +4,50 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils/utils";
 import { Eye, EyeOff } from "lucide-react";
-import * as React from "react";
-import { forwardRef, InputHTMLAttributes } from "react";
+import {
+  forwardRef,
+  InputHTMLAttributes,
+  isValidElement,
+  ReactNode,
+  useState,
+} from "react";
 
-type SensitiveInputProps = InputHTMLAttributes<HTMLInputElement>;
+type SensitiveInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  value: string | ReactNode;
+};
 
 const SensitiveInput = forwardRef<HTMLInputElement, SensitiveInputProps>(
-  ({ className, ...props }, ref) => {
-    const [isRevealed, setIsRevealed] = React.useState(false);
+  ({ className, value, ...props }, ref) => {
+    const [isRevealed, setIsRevealed] = useState<boolean>(false);
+    const isReactNode: boolean = isValidElement(value);
     return (
       <div className="group w-full relative flex items-center">
-        <Input
-          ref={ref}
-          className={cn(
-            "pr-10 md:pr-4 transition-all duration-200 transform-gpu",
-            !isRevealed && "blur-[4px] hover:blur-none focus:blur-none",
-            className
-          )}
-          type="text"
-          {...props}
-        />
+        {isReactNode ? (
+          // Rendering for ReactNode
+          <div
+            className={cn(
+              "w-full px-3 py-2 rounded-md border border-input bg-background text-sm ring-offset-background",
+              "pr-10 md:pr-4 transition-all duration-200 transform-gpu",
+              !isRevealed && "blur-[4px] hover:blur-none focus:blur-none",
+              className
+            )}
+          >
+            {value}
+          </div>
+        ) : (
+          // If it's a string, use it as the input value
+          <Input
+            ref={ref}
+            className={cn(
+              "pr-10 md:pr-4 transition-all duration-200 transform-gpu",
+              !isRevealed && "blur-[4px] hover:blur-none focus:blur-none",
+              className
+            )}
+            type="text"
+            value={value as string}
+            {...props}
+          />
+        )}
         <Button
           className="md:hidden absolute right-0 h-full px-3 py-2 hover:bg-transparent"
           type="button"
