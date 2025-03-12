9e284e58e1f40bbec741b34eac61263a2b5ecd97
Lee <liam@fascinated.cc>
2025-03-12 04:41:06 +0000

    add toast on error for query client

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Logger from "@/lib/logger";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
+import { toast } from "sonner";
 
 type Props = {
   children: ReactNode;
@@ -11,10 +13,23 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 30, // 30 seconds
+      gcTime: 1000 * 60 * 30, // 30 minutes
       refetchInterval: 1000 * 60, // 1 minutes
       refetchIntervalInBackground: false,
-      retry: true,
-      retryDelay: 1000 * 5, // 5 seconds
+      retry: 8, // 8 retries
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000), // Exponential backoff
+    },
+  },
+});
+
+// Add global error handler
+queryClient.setDefaultOptions({
+  mutations: {
+    onError: (error, variables, context) => {
+      toast.error("An error occurred whilst performing an action.", {
+        description: error.message,
+      });
+      Logger.error(error);
     },
   },
 });
