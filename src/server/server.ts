f5bd613610b5e4250ceb918bf36541922e3d334b
Lee <liam@fascinated.cc>
2025-03-14 03:12:23 +0000

    fix hot reloading

--- src/server/server.ts
+++ src/server/server.ts
@@ -18,6 +18,7 @@ const app = next({ dev, conf: nextConfig, turbopack: dev, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
+
   const server = createServer(async (request, response) => {
     const parsedUrl = parse(request.url!, true);
 
@@ -35,6 +36,7 @@ app.prepare().then(() => {
       );
     }
   }).listen(port);
+  app.options.httpServer = server;
 
   server.on("listening", async () => {
     await runMigrations();
